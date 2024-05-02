ac98f4f5cfea710274b9da46c103e7df880d9757
rana tarek <132678372+rana854@users.noreply.github.com>
2024-05-03 00:54:12 +0300

    Update Jenkinsfile.groovy

--- Django project/myproject/Jenkinsfile.groovy
+++ Django project/myproject/Jenkinsfile.groovy
@@ -7,10 +7,21 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/rana854/cicd-project-1.git'
             }
         }
-       
+      
+        stage('Install Dependencies') {
+            steps {
+              Python install -r Django project/myproject/requirements.txt
+                
+            }
+        }
         stage('Run Tests') {
             steps {
-                bat 'python cicd-project-1/Django project/myproject/manage.py test' // Use 'bat' instead of 'sh' for Windows
+              python manage.py test
+            }
+        }
+        stage('Run Tests') {
+            steps {
+                bat 'python cicd-project-1/Django project/myproject/manage.py test'
             }
         }
         stage('Deploy') {

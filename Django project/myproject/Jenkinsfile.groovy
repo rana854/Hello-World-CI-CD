e6b3f4af0b326cde7f5378da9c908bfda6fd72a0
Rana Tarek <132678372+rana854@users.noreply.github.com>
2024-11-10 13:51:38 +0200

    Update Jenkinsfile.groovy

--- Django project/myproject/Jenkinsfile.groovy
+++ Django project/myproject/Jenkinsfile.groovy
@@ -21,7 +21,7 @@ pipeline {
                 script {
                     // Build the Docker image with a specific tag
                     def imageTag = "${DOCKER_USERNAME}/${IMAGE_NAME}:latest"
-                    bat "docker build -t ${imageTag} -f Django\\ project\\myproject\\Dockerfile ."
+                    bat "docker build -t ${imageTag} -f Django project/myproject/Dockerfile ."
                     
                 }
             }

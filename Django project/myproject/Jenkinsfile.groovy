3d32eeae162a380fb8ba691243162097198fab62
Rana Tarek <132678372+rana854@users.noreply.github.com>
2024-11-10 15:09:33 +0200

    Update Jenkinsfile.groovy

--- Django project/myproject/Jenkinsfile.groovy
+++ Django project/myproject/Jenkinsfile.groovy
@@ -5,7 +5,10 @@ pipeline {
         // Docker Hub username and password (insecure, avoid in production)
         DOCKER_USERNAME = 'ranatarek'
         DOCKER_PASSWORD = 'Rana3940498'
-        IMAGE_NAME = 'pipline_docker_image'
+        IMAGE_NAME = 'pipline_docker_image2'
+        MINIKUBE_PROFILE = 'minikube' // Set the name of your Minikube profile if you have one
+        K8S_DEPLOYMENT_NAME = 'myapp-deployment'  // Change to your Kubernetes deployment name
+        K8S_SERVICE_NAME = 'myapp-service'        // Change to your Kubernetes service name
     }
 
     stages {
@@ -44,6 +47,27 @@ pipeline {
                     def imageTag = "${DOCKER_USERNAME}/${IMAGE_NAME}:latest"
                     // Pubat the Docker image to Docker Hub
                     bat "docker push ${imageTag}"
+                }
+            }
+        }
+
+         stage('Deploy to Minikube') {
+            steps {
+                script {
+                    // Set the Minikube environment
+                    bat "minikube start --driver=docker --profile ${MINIKUBE_PROFILE}" // Ensure Minikube is started
+                    
+                    // Set Kubeconfig to point to Minikube
+                    bat "kubectl config use-context minikube"
+
+                    // Deploy the application to Minikube Kubernetes cluster
+                    // Define Kubernetes deployment and service YAML files
+                    bat "kubectl apply -f k8s/deployment.yaml" // Path to your deployment.yaml file
+                    bat "kubectl apply -f k8s/service.yaml"     // Path to your service.yaml file
+                    
+                    // Optionally, expose the service (you can skip this if you already exposed the service)
+                    bat "kubectl expose deployment ${K8S_DEPLOYMENT_NAME} --type=ClusterIP --name=${K8S_SERVICE_NAME}"
+
                 }
             }
         }
